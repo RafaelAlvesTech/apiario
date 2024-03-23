058eeddfe4337459c8088013c6f5a2b466cd5a6b
Rafael Alves <rafaelpereiraalves@ufba.br>
2024-03-23 12:06:16 -0300

    Django add

---
+++ .history/apiario/setup_20240323120246.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="apiario",
+    version="0.1.0",
+    packages=["apiario"],
+    install_requires=[
+        "django"],
+)
---
+++ .history/setup_20240323115614.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name="apiario",
+    version="0.1",
+    packages=['apiario'],
+    install_requires=[
+        'Django'],
+)
