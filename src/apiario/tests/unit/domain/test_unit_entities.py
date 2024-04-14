e4001ea24be2630d011a292ad169cf932f9135f4
RafaelAlvesTech <rafaelpereiraalves@ufba.br>
2024-04-14 22:56:40 +0000

    unit test apario incomplete

---
+++ src/apiario/tests/unit/domain/test_unit_entities.py
@@ -0,0 +1,9 @@
+from dataclasses import is_dataclass
+from datetime import datetime
+import unittest
+from apiario.domain.entities import Apiario
+
+class TestApiarioUnit(unittest.TestCase):
+    def test_if_is_a_dataclass(self):
+        pass
+        
