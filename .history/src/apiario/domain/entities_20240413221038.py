3bf1ab40e172ac1db9179adf6408287d5cc2c967
RafaelAlvesTech <rafaelpereiraalves@ufba.br>
2024-04-14 11:15:16 +0000

    remove

---
+++ .history/src/apiario/domain/entities_20240413221038.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclasses
+
+@dataclass()
+class Apiario:
+
+    id: int
+    id_usuario: int
+    status: str = A
+    atividade: str = T
+    nome: str 
+    estrutura: str = F
+    cep: 
+    
+
